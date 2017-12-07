0743118cb30a947e2b024e76b48f83db77bba65a
LAPTOP-055GPRQV\Saddleback <kellymcbean@gmail.com>
2017-12-07 12:26:00 -0800

    Added the Shield up test

--- TestedTrek/src/StarTrek/Shield.java
+++ TestedTrek/src/StarTrek/Shield.java
@@ -2,10 +2,18 @@ package StarTrek;
 
 public class Shield {
 
-    private boolean upStatus = false;
+    private boolean isUp = false;
 
     public void raise() {
-        upStatus = true;
-
+        isUp = true;
     }
+
+    public void lower() {
+        isUp = false;
+    }
+
+    public boolean isUp() {
+        return isUp;
+    }
+
 }
--- TestedTrek/src/Tests/ShieldTest.java
+++ TestedTrek/src/Tests/ShieldTest.java
@@ -1,39 +1,18 @@
 package Tests;
 
-import StarTrek.Game;
 import StarTrek.Shield;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-//Test Git 2
 public class ShieldTest {
-    private Game game;
-    private MockGalaxy context;
-
-    @Before
-    public void setUp() {
-        game = new Game();
-        context = new MockGalaxy();
-    }
 
     @Test
     public void RaiseShield() {
         Shield shield = new Shield();
-
-//        Assert.assertTrue(shield.statusUp());
-
-
-//        shield.raise();
-
-
-
-
-
+        Assert.assertFalse(shield.isUp());
+        shield.raise();
+        Assert.assertTrue(shield.isUp());
     }
-
-
-
 
 }
 
